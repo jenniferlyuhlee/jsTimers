84c85cf02550ef33b1daf0f6cd16d5b7d41c8b22
jlee4383 <78133100+jlee4383@users.noreply.github.com>
2024-01-17 01:21:41 -0800

    initial commit

---
+++ countdown.js
@@ -0,0 +1,13 @@
+function countDown(num){
+    let timer = setInterval(function(){
+        num--;
+        if (num > 0){
+            console.log(num);
+        }
+        else{
+            clearInterval(timer);
+            console.log('DONE!');
+        }
+    }, 1000);
+
+}
---
+++ randomGame.js
@@ -0,0 +1,13 @@
+function randomGame(){
+    let counter = 1;
+    let timer = setInterval(function(){
+        let randomNum = Math.random();
+        console.log(randomNum);
+        if (randomNum > .75){
+            clearInterval(timer);
+            console.log (`It took ${counter} tries`);
+        }
+        counter++;
+    }, 1000)
+    
+}
